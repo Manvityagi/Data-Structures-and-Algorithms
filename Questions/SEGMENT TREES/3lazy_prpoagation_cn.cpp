da627e681f1d90d54b00cce44ebbbf2be23d4095
Manvi Tyagi <manvityagi770@gmail.com>
2019-10-17 01:51:41 +0530

    happy 50 mins to complete sum of squares segtree

---
+++ Questions/SEGMENT TREES/3lazy_prpoagation_cn.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void buildTree(int *arr, int *tree, int start, int end, int treenode)
+{
+    //base case
+    if (start == end)
+    {
+        tree[treenode] = arr[start];
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    buildTree(arr, tree, start, mid, 2 * treenode);
+    buildTree(arr, tree, mid + 1, end, 2 * treenode + 1);
+
+    tree[treenode] = tree[2 * treenode] + tree[2 * treenode + 1];
+}
+
+void updateSegmentTreeLazy(int *tree, int *lazy, int treeIndex, int start, int end, int qs, int qe, int inc)
+{
+    //base case
+    if (start > end || qs > qe)
+        return;
+    //update tree a/c to lazy tree before moving on to other operations
+    if (lazy[treeIndex] != 0)
+    {
+        tree[treeIndex] += lazy[treeIndex];
+        if (start != end)
+        {
+            lazy[2 * treeIndex] += lazy[treeIndex];
+            lazy[2 * treeIndex + 1] += lazy[treeIndex];
+        }
+        lazy[treeIndex] = 0;
+    }
+    //no overlap
+    if (qe < start || qs > end)
+        return;
+
+    //complete overlap
+    if (qs <= start && qe >= end)
+    {
+        tree[treeIndex] += inc;
+        if (start != end)
+        {
+            lazy[2 * treeIndex] += inc;
+            lazy[2 * treeIndex + 1] += inc;
+        }
+        return;
+    }
+    //partial overlap
+    int mid = (start + end) / 2;
+    updateSegmentTreeLazy(tree, lazy, 2 * treeIndex, start, mid, qs, qe, inc);
+    updateSegmentTreeLazy(tree, lazy, 2 * treeIndex + 1, mid + 1, end, qs, qe, inc);
+    tree[treeIndex] = tree[2 * treeIndex] + tree[2 * treeIndex + 1];
+}
+
+int query(int *tree, int *lazy, int s, int e, int treeIndex, int left, int right)
+{
+    if (s > right || e < left)
+        return 0;
+
+    if (lazy[treeIndex] != 0)
+    {
+        tree[treeIndex] += lazy[treeIndex];
+        if (s != e)
+        {
+            lazy[2 * treeIndex] += lazy[treeIndex];
+            lazy[2 * treeIndex + 1] += lazy[treeIndex];
+        }
+        lazy[treeIndex] = 0;
+    }
+
+    if (left <= s && right >= e)
+        return tree[treeIndex];
+
+    int mid = s + (e - s) / 2;
+    int ans1 = query(tree, lazy, s, mid, 2 * treeIndex, left, right);
+    int ans2 = query(tree, lazy, mid + 1, e, 2 * treeIndex + 1, left, right);
+
+    return ans1 + ans2;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *tree = new int[18];
+
+    buildTree(arr, tree, 0, 8, 1);
+
+    for (int i = 1; i <= 18; i++)
+        cout << tree[i] << endl;
+
+    // update(arr,tree,0,8,4,10,1);
+
+    // for(int i = 1; i <= 18; i++)
+    //     cout << tree[i] << endl;
+
+    // cout << query(tree,0,8,1,2,7);
+}
---
+++ Questions/SEGMENT TREES/4sum_of_squares.cpp
@@ -0,0 +1,179 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+void buildTree(int *arr, int *tree, int low, int high, int treeIndex)
+{
+	//base case - leaf node
+	if (low == high)
+	{
+		tree[treeIndex] = arr[low] * arr[low];
+		return;
+	}
+	int mid = low + (high - low) / 2;
+	buildTree(arr, tree, low, mid, 2 * treeIndex);
+	buildTree(arr, tree, mid + 1, high, 2 * treeIndex + 1);
+
+	tree[treeIndex] = tree[2 * treeIndex] + tree[2 * treeIndex + 1];
+}
+
+class treenode
+{
+public:
+	int ss;
+	int s;
+
+	treenode()
+	{
+		ss = 0;
+		s = 0;
+	}
+};
+
+class lazynode
+{
+public:
+	int val;
+	char type;
+
+	lazynode()
+	{
+		val = 0;
+		type = ' ';
+	}
+};
+
+void update(treenode *tree, lazynode *lazy, int low, int high, int qs, int qe, int ut, int val, int treeindex)
+{
+	if (low > high || qs > qe)
+		return;
+
+	//first of all, do any pending updates on current treenode
+	if (lazy[treeindex].val != 0)
+	{
+		//chnage tree's value according to type of update
+
+		//if type inc
+		if (lazy[treeindex].type == 'i')
+		{
+			tree[treeindex].ss += 2 * val * tree[treeindex].s + val * val * (high - low + 1);
+			tree[treeindex].s += val * (high - low + 1);
+		}
+		//if type change
+		if (lazy[treeindex].type == 'c')
+		{
+			tree[treeindex].ss = val * val * (high - low + 1);
+			tree[treeindex].s = val * (high - low + 1);
+		}
+		//update children in lazy tree
+		if (low != high)
+		{
+			lazy[2 * treeindex].val = lazy[treeindex].val;
+			lazy[2 * treeindex + 1].val = lazy[treeindex].val;
+			lazy[treeindex].type = lazy[treeindex].type;
+		}
+		//last step
+		lazy[treeindex].val = 0;
+	}
+
+	//no overlap
+	if (qe < low || qs > high)
+		return;
+
+	//complete overlap
+	if (qs <= low && qe >= high)
+	{
+		if (ut == 1) //inc
+		{
+			tree[treeindex].ss += 2 * val * tree[treeindex].s + val * val * (high - low + 1);
+			tree[treeindex].s += val * (high - low + 1);
+		}
+		else //changing
+		{
+			tree[treeindex].ss = val * val * (high - low + 1);
+			tree[treeindex].s = val * (high - low + 1);
+		}
+
+		return;
+	}
+
+	//partial overlap
+	int mid = low + (high - low) / 2;
+	update(tree, lazy, low, mid, qs, qe, ut, val, 2 * treeindex);
+	update(tree, lazy, mid + 1, high, qs, qe, ut, val, 2 * treeindex + 1);
+
+	tree[treeindex].ss = tree[2 * treeindex].ss + tree[2 * treeindex + 1].ss;
+	tree[treeindex].s = tree[2 * treeindex].s + tree[2 * treeindex + 1].s;
+}
+
+int query(lazynode *lazy, treenode *tree, int low, int high, int qs, int qe, int val, int treeindex)
+{
+	if (low > high || qs > qe)
+		return 0;
+
+	//first of all, do any pending updates on current treenode
+	if (lazy[treeindex].val != 0)
+	{
+		//change tree's value according to type of update
+
+		//if type inc
+		if (lazy[treeindex].type == 'i')
+		{
+			tree[treeindex].ss += 2 * val * tree[treeindex].s + val * val * (high - low + 1);
+			tree[treeindex].s += val * (high - low + 1);
+		}
+		//if type change
+		if (lazy[treeindex].type == 'c')
+		{
+			tree[treeindex].ss = val * val * (high - low + 1);
+			tree[treeindex].s = val * (high - low + 1);
+		}
+		//update children in lazy tree
+		if (low != high)
+		{
+			lazy[2 * treeindex].val = lazy[treeindex].val;
+			lazy[2 * treeindex + 1].val = lazy[treeindex].val;
+			lazy[treeindex].type = lazy[treeindex].type;
+		}
+		//last step
+		lazy[treeindex].val = 0;
+	}
+
+	if (qs <= low && qe >= high)
+		return tree[treeindex].ss;
+
+	int mid = low + (high - low) / 2;
+	int ans1 = query(lazy,tree,low,mid,qs,qe,val,2*treeindex);
+	int ans2 = query(lazy,tree,mid+1,high,qs,qe,val,2*treeindex+1);
+	return ans1 + ans2;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, q;
+		cin >> n >> q;
+		int *arr = new int[n];
+		int *tree = new int[3 * n];
+		for (int i = 0; i < n; i++)
+			cin >> arr[i];
+
+		buildTree(arr, tree, 0, n - 1, 1);
+
+		while (q--)
+		{
+			int type, st, nd;
+			cin >> type >> st >> nd;
+		}
+	}
+	return 0;
+}
